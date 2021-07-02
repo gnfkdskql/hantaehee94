e5dfb6c066183148ca048012435f135147d5425c
gnfkdskql <70249565+gnfkdskql@users.noreply.github.com>
2021-07-02 15:39:49 +0900

    ver1.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { hydrate, render } from "react-dom";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -8,3 +9,10 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById("root")
 );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
